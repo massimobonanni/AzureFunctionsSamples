f55782f89efa3ea3ca56747756315aac974574d3
Massimo Bonanni <massimo.bonanni@tiscali.it>
2020-12-29 13:35:39 +0100

    Add tests for multiple messages in queue

--- AppConfigSyncFunction/AppConfigSyncFunction.Tests/AppConfigSyncTest.cs
+++ AppConfigSyncFunction/AppConfigSyncFunction.Tests/AppConfigSyncTest.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Moq;
 using System;
 using System.Collections.Generic;
+using System.Reflection.Metadata;
 using System.Threading.Tasks;
 using Xunit;
 using IMSLogger = Microsoft.Extensions.Logging.ILogger;
@@ -112,7 +113,7 @@ namespace AppConfigSyncFunction.Tests
         }
 
         [Fact]
-        public async Task AppConfigSyncFunction_OneMessagesInQueue_UpdateMessage_UpdateFromPrimaryToSecondary()
+        public async Task AppConfigSyncFunction_OneMessageInQueue_UpdateMessage_UpdateFromPrimaryToSecondary()
         {
             var appConfigSyncServiceMock = new Mock<IAppConfigurationSyncService>();
             var queueServiceMock = new Mock<IEventsService>();
@@ -158,6 +159,123 @@ namespace AppConfigSyncFunction.Tests
 
             appConfigSyncServiceMock
                .Verify(e => e.UpsertToSecondary(modifiedEvent, default), Times.Once);
+        }
+
+        [Fact]
+        public async Task AppConfigSyncFunction_OneMessageInQueue_DeleteMessage_DeleteFromPrimaryToSecondary()
+        {
+            var appConfigSyncServiceMock = new Mock<IAppConfigurationSyncService>();
+            var queueServiceMock = new Mock<IEventsService>();
+            var configurationMock = new Mock<IConfiguration>();
+            var loggerMock = new Mock<IMSLogger>();
+
+            var timeInfo = TestUtility.GenerateTimerInfo();
+
+            var deletedEvent = TestUtility.CreateKeyValueDeletedEvent();
+
+            queueServiceMock
+                .SetupSequence(e => e.ReceiveEventsAsync(It.IsAny<int>()))
+                .ReturnsAsync(new List<Event>() { deletedEvent })
+                .ReturnsAsync(new List<Event>());
+
+            queueServiceMock
+                .Setup(e => e.DeleteEventAsync(deletedEvent))
+                .Verifiable();
+
+            appConfigSyncServiceMock
+                .Setup(e => e.Connect(null, null))
+                .Verifiable();
+
+            appConfigSyncServiceMock
+                .Setup(e => e.DeleteFromSecondary(deletedEvent, default))
+                .ReturnsAsync(true)
+                .Verifiable();
+
+            var target = new AppConfigSync(appConfigSyncServiceMock.Object,
+                queueServiceMock.Object,
+                configurationMock.Object);
+
+            await target.AppConfigSyncFunction(timeInfo, loggerMock.Object);
+
+            queueServiceMock
+                .Verify(e => e.ReceiveEventsAsync(It.IsAny<int>()), Times.Exactly(2));
+
+            queueServiceMock
+                .Verify(e => e.DeleteEventAsync(deletedEvent), Times.Once);
+
+            appConfigSyncServiceMock
+                .Verify(e => e.Connect(null, null), Times.Once);
+
+            appConfigSyncServiceMock
+               .Verify(e => e.DeleteFromSecondary(deletedEvent, default), Times.Once);
+        }
+
+        [Fact]
+        public async Task AppConfigSyncFunction_TwoMessagesInQueue_DeleteAndModifiedMessages_DeleteFromPrimaryToSecondaryThenUpdateFromPrimaryToSecondary()
+        {
+            var appConfigServiceMethodsOrder = string.Empty;
+
+            var appConfigSyncServiceMock = new Mock<IAppConfigurationSyncService>();
+            var queueServiceMock = new Mock<IEventsService>();
+            var configurationMock = new Mock<IConfiguration>();
+            var loggerMock = new Mock<IMSLogger>();
+
+            var timeInfo = TestUtility.GenerateTimerInfo();
+
+            var deletedEvent = TestUtility.CreateKeyValueDeletedEvent();
+            var modifiedEvent = TestUtility.CreateKeyValueModifiedEvent();
+
+            queueServiceMock
+                .SetupSequence(e => e.ReceiveEventsAsync(It.IsAny<int>()))
+                .ReturnsAsync(new List<Event>() { deletedEvent, modifiedEvent })
+                .ReturnsAsync(new List<Event>());
+
+            queueServiceMock
+                .Setup(e => e.DeleteEventAsync(deletedEvent))
+                .Verifiable();
+            queueServiceMock
+                .Setup(e => e.DeleteEventAsync(modifiedEvent))
+                .Verifiable();
+
+            appConfigSyncServiceMock
+                .Setup(e => e.Connect(null, null))
+                .Verifiable();
+
+            appConfigSyncServiceMock
+                .Setup(e => e.DeleteFromSecondary(deletedEvent, default))
+                .ReturnsAsync(true)
+                .Callback(() => appConfigServiceMethodsOrder += "Delete")
+                .Verifiable();
+            appConfigSyncServiceMock
+                .Setup(e => e.UpsertToSecondary(modifiedEvent, default))
+                .ReturnsAsync(true)
+                .Callback(() => appConfigServiceMethodsOrder += "Upsert")
+                .Verifiable();
+
+            var target = new AppConfigSync(appConfigSyncServiceMock.Object,
+                queueServiceMock.Object,
+                configurationMock.Object);
+
+            await target.AppConfigSyncFunction(timeInfo, loggerMock.Object);
+
+            queueServiceMock
+                .Verify(e => e.ReceiveEventsAsync(It.IsAny<int>()), Times.Exactly(2));
+
+            queueServiceMock
+                .Verify(e => e.DeleteEventAsync(deletedEvent), Times.Once);
+            queueServiceMock
+                .Verify(e => e.DeleteEventAsync(modifiedEvent), Times.Once);
+
+            appConfigSyncServiceMock
+                .Verify(e => e.Connect(null, null), Times.Once);
+
+            appConfigSyncServiceMock
+               .Verify(e => e.DeleteFromSecondary(deletedEvent, default), Times.Once);
+            appConfigSyncServiceMock
+               .Verify(e => e.UpsertToSecondary(modifiedEvent, default), Times.Once);
+
+            Assert.Equal("DeleteUpsert", appConfigServiceMethodsOrder);
+            Assert.Equal("DeleteUpsert", appConfigServiceMethodsOrder);
         }
         #endregion [ AppConfigSyncFunction ]
     }
--- AppConfigSyncFunction/AppConfigSyncFunction.Tests/TestUtility.cs
+++ AppConfigSyncFunction/AppConfigSyncFunction.Tests/TestUtility.cs
@@ -37,5 +37,24 @@ namespace AppConfigSyncFunction.Tests
             };
         }
 
+        internal static Event CreateKeyValueDeletedEvent()
+        {
+            return new Event()
+            {
+                EventTime = DateTime.Now,
+                EventType = Event.KeyValueDeleted,
+                Id = Guid.NewGuid().ToString(),
+                DataVersion = "1",
+                MetadataVersion = "1",
+                Subject = "https://primaryappconfig.azconfig.io/kv/myConfigValue?label=%00&api-version=1.0",
+                Topic = "/subscriptions/60504780-44c1-4d01-b84f-baca1d9239ed/resourceGroups/appconfigurationreplication-rg/providers/microsoft.appconfiguration/configurationstores/primaryappconfig",
+                Data = new Data()
+                {
+                    etag = Guid.NewGuid().ToString(),
+                    key = "Key",
+                    label = "label"
+                }
+            };
+        }
     }
 }
